e75d16d870412ebd5b68035092d62511356a486e
Mateus Martinez Rosa <mayeusitau.unibanco@gmail.com>
2023-07-10 09:25:18 -0300

    finalizando a venda

--- src/Views/Sale.js
+++ src/Views/Sale.js
@@ -2,97 +2,105 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Sale = () => {
-    const [produtos, setProdutos] = useState([]);
-    const [produtoSelecionado, setProdutoSelecionado] = useState(null);
-    const [vendas, setVendas] = useState([]);
-    const [totalCompra, setTotalCompra] = useState(0);
-    const [totalImpostos, setTotalImpostos] = useState(0);
+    const [products, setProducts] = useState([]);
+    const [productSelected, setProductSelected] = useState(null);
+    const [sales, setSales] = useState([]);
+    const [totalBuy, setTotalBuy] = useState(0);
+    const [totalTaxes, setTotalTaxes] = useState(0);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
     useEffect(() => {
-        carregarProdutos();
+        loadProducts();
     }, []);
 
-    const carregarProdutos = async () => {
+    const loadProducts = async () => {
         try {
             const API_URL = 'http://localhost:8000';
             const response = await axios.get(API_URL + '/products');
-            const produtosCarregados = response.data;
-            console.log(produtosCarregados);
-            setProdutos(produtosCarregados);
+            const productsLoaded = response.data;
+            console.log(productsLoaded);
+            setProducts(productsLoaded);
         } catch (error) {
-            console.error('Erro ao carregar produtos:', error);
+            console.error('Erro ao carregar products:', error);
         }
     };
 
-    const adicionarVenda = () => {
-        if (produtoSelecionado) {
-            const venda = {
-                id: produtoSelecionado.product_id,
-                nome: produtoSelecionado.product_name,
-                valor: produtoSelecionado.product_price,
-                quantidade: produtoSelecionado.quantidade,
-                imposto: produtoSelecionado.prod_type_percentage,
-                valorTotal: produtoSelecionado.product_price * produtoSelecionado.quantidade,
+    const addSale = () => {
+        if (productSelected) {
+            const sale = {
+                product_id: productSelected.product_id,
+                name: productSelected.product_name,
+                value: productSelected.product_price,
+                quantity: productSelected.quantity,
+                tax: productSelected.prod_type_percentage,
+                totalAmount: productSelected.product_price * productSelected.quantity,
             };
-            const impostoTotal = venda.imposto * venda.quantidade;
-            setVendas([...vendas, venda]);
-            setTotalCompra(totalCompra + venda.valorTotal);
-            setTotalImpostos(totalImpostos + impostoTotal);
-            setProdutoSelecionado(null);
+            const taxTotal = sale.tax * sale.quantity;
+            setSales([...sales, sale]);
+            setTotalBuy(totalBuy + sale.totalAmount);
+            setTotalTaxes(totalTaxes + taxTotal);
+            setProductSelected(null);
         }
     };
 
-    const removerVenda = (venda) => {
-        const impostoTotal = venda.imposto * venda.quantidade;
-        const vendasAtualizadas = vendas.filter((v) => v !== venda);
-        setVendas(vendasAtualizadas);
-        setTotalCompra(totalCompra - venda.valorTotal);
-        setTotalImpostos(totalImpostos - impostoTotal);
+    const removeSale = (sale) => {
+        const taxTotal = sale.tax * sale.quantity;
+        const salesUpdated = sales.filter((v) => v !== sale);
+        setSales(salesUpdated);
+        setTotalBuy(totalBuy - sale.totalAmount);
+        setTotalTaxes(totalTaxes - taxTotal);
     };
 
-    const handleProdutoSelecionado = (produto) => {
-        setProdutoSelecionado(produto);
+    const handleProdutoSelecionado = (product) => {
+        setProductSelected(product);
     };
 
-    const formatarMoeda = (valor) => {
-        return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    const formatCurrency = (value) => {
+        return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
     };
 
     const handleQuantidadeChange = (event) => {
-        const quantidade = parseInt(event.target.value, 10);
-        setProdutoSelecionado({ ...produtoSelecionado, quantidade });
+        const quantity = parseInt(event.target.value, 10);
+        setProductSelected({ ...productSelected, quantity });
     };
 
-    const finalizarVenda = async () => {
+    const finalizeSale = async () => {
         try {
-            console.log(vendas);
-            console.log(totalCompra);
+            console.log(sales);
+            console.log(totalBuy);
             const API_URL = 'http://localhost:8000';
-            const response = await axios.post(API_URL + '/sales', { produtos: vendas });
+            const response = await axios.post(API_URL + '/sales', sales);
             console.log('Venda finalizada:', response.data);
-            // Reset the state after successful sale
-            setVendas([]);
-            setTotalCompra(0);
-            setTotalImpostos(0);
+            setSales([]);
+            setTotalBuy(0);
+            setTotalTaxes(0);
+
+            setShowSuccessMessage(true);
+            setTimeout(() => {
+                setShowSuccessMessage(false);
+            }, 3000);
         } catch (error) {
-            console.error('Erro ao finalizar a venda:', error);
+            console.error('Error when completing the sale:', error);
         }
     };
     return (
         <div className="container">
             <h1>Vendas</h1>
+            {showSuccessMessage && (
+                <div className="alert alert-success mb-3">Produto cadastrado com sucesso!</div>
+            )}
             <div className="form-group mb-3">
                 <label>Produtos:</label>
                 <ul className="list-group">
-                    {produtos.map((produto) => (
+                    {products.map((product) => (
                         <li
-                            key={produto.id}
-                            className={`list-group-item ${produto === produtoSelecionado ? 'active' : ''}`}
-                            onClick={() => handleProdutoSelecionado(produto)}
+                            key={product.product_id}
+                            className={`list-group-item ${product === productSelected ? 'active' : ''}`}
+                            onClick={() => handleProdutoSelecionado(product)}
                             style={{ cursor: 'pointer' }}
                         >
-                            {produto.product_name} - R${formatarMoeda(produto.product_price)} - <small><b>({produto.prod_type_name})
-                                Imp: %{produto.prod_type_percentage} </b></small>
+                            {product.product_name} - R${formatCurrency(product.product_price)} - <small><b>({product.prod_type_name})
+                                Imp: %{product.prod_type_percentage} </b></small>
                         </li>
                     ))}
                 </ul>
@@ -102,49 +110,49 @@ const Sale = () => {
                 <input
                     type="number"
                     className="form-control"
-                    value={produtoSelecionado ? produtoSelecionado.quantidade : ''}
+                    value={productSelected ? productSelected.quantity : ''}
                     onChange={handleQuantidadeChange}
                     min="1"
-                    disabled={!produtoSelecionado}
+                    disabled={!productSelected}
                 />
             </div>
             <div className='row'>
                 <div className='col-3'>
                     <button
                         className="btn btn-success mb-3"
-                        onClick={adicionarVenda}
-                        disabled={!produtoSelecionado}
+                        onClick={addSale}
+                        disabled={!productSelected}
                     >+
                     </button>
                 </div>
                 <div className='col-md-3'>
-                    <h6>Total Compra: {formatarMoeda(totalCompra)}</h6> <h6>Total Impostos: {formatarMoeda(totalImpostos)}</h6>
+                    <h6>Total Compra: {formatCurrency(totalBuy)}</h6> <h6>Total Impostos: {formatCurrency(totalTaxes)}</h6>
                 </div>
                 <div className='col-6'>
-                    <button className="btn btn-success mb-3" onClick={finalizarVenda}>Finalizar Venda</button>
+                    <button className="btn btn-success mb-3" onClick={finalizeSale}>Finalizar Venda</button>
                 </div>
             </div>
 
 
             {
-                vendas.length === 0 &&
+                sales.length === 0 &&
                 <div className='col-12'>
                     <div className='alert alert-info mb-3'>
                         Nenhum produto no carrinho.
                     </div>
                 </div>
             }
-            {vendas.map((venda, index) => (
-                <div key={venda.id} className="card mb-3">
+            {sales.map((sale, index) => (
+                <div key={sale.id} className="card mb-3">
                     <div className="card-body">
-                        <h5 className="card-title">{venda.nome}</h5>
-                        <p>Valor: R${venda.valor}</p>
-                        <p>Quantidade {venda.quantidade}</p>
-                        <p>Total: {formatarMoeda((venda.valor * venda.quantidade))}</p>
-                        <p>Imposto: {formatarMoeda((venda.imposto * venda.quantidade))}</p>
+                        <h5 className="card-title">{sale.name}</h5>
+                        <p>Valor: R${sale.value}</p>
+                        <p>Quantidade {sale.quantity}</p>
+                        <p>Total: {formatCurrency((sale.value * sale.quantity))}</p>
+                        <p>Imposto: {formatCurrency((sale.tax * sale.quantity))}</p>
                         <button
                             className="btn btn-outline-danger btn-sm"
-                            onClick={() => removerVenda(venda)}
+                            onClick={() => removeSale(sale)}
                         >
                             Remove
                         </button>
